9ad70a44c0b327c6aa88a2118b82df6a3f704ce6
Slattz <Slattz@users.noreply.github.com>
2019-01-08 18:54:23 +0000

    Implement beter File class

--- Headers/file.h
+++ Headers/file.h
@@ -14,9 +14,13 @@ class File
     public:
         File();
         ~File();
-        File(const QString &fileName, QIODevice::OpenMode openmode = QFile::ReadOnly);
+        File(const QString &fileName, QIODevice::OpenMode openmode = QFile::ReadOnly, bool warning = true);
+        void Close(void);
         bool Write(quint32 offset, quint32 bytes, QDataStream::ByteOrder bo = QDataStream::BigEndian);
-        quint32 Read(quint32 offset, QDataStream::ByteOrder bo);
+        bool Write(quint32 offset, QByteArray bytes, QDataStream::ByteOrder bo = QDataStream::BigEndian);
+        bool Append(QByteArray bytes, QDataStream::ByteOrder bo = QDataStream::BigEndian);
+        quint32 Read(quint32 offset, QDataStream::ByteOrder bo = QDataStream::BigEndian);
+        QByteArray* Read(quint32 offset, int size, QDataStream::ByteOrder bo = QDataStream::BigEndian);
         bool Exists();
         QByteArray getHash();
         QFile *m_file = nullptr;
@@ -25,6 +29,7 @@ class File
         bool m_exists = false;
         qint64 m_size = 0;
         QCryptographicHash m_md5Hash;
+        QIODevice::OpenMode m_openmode;
 
 };
 
--- Sources/file.cpp
+++ Sources/file.cpp
@@ -1,27 +1,38 @@
 #include "Headers/file.h"
-#include "Headers/mainwindow.h"
-#include "ui_mainwindow.h"
 
 File::File() : m_md5Hash(QCryptographicHash::Md5) { }
-File::~File() { }
+File::~File() {
+    this->Close();
+    m_file = nullptr;
+    m_size = 0;
+    m_exists = false;
+}
 
-File::File(const QString &filePath, QIODevice::OpenMode openmode)
+File::File(const QString &filePath, QIODevice::OpenMode openmode, bool warning)
     : File()
 {
     m_file = new QFile(filePath);
-    if (m_file->exists() && m_file->open(openmode)) {
+    if (m_file->open(openmode)) {
         m_exists = true;
+        m_openmode = openmode;
         m_size = m_file->size();
         m_md5Hash.addData(m_file);
         qDebug().noquote() << "File Path: " + filePath + "\nFile MD5: " + m_md5Hash.result().toHex();
     }
 
     else {
-        QMessageBox::warning(nullptr, "Failed Opening File", "Failed to open file: " + filePath + "!\nSome features may be disabled as a result.");
+        if (warning) QMessageBox::warning(nullptr, "Failed Opening File", "Failed to open file: " + filePath + "!\nSome features may be disabled as a result.");
         m_exists = false;
         if (m_file)
             delete m_file;
     }
+}
+
+void File::Close(void) {
+    if (!m_exists) return;
+
+    if ((m_openmode & QFile::WriteOnly)) m_file->flush();
+    m_file->close();
 }
 
 bool File::Write(quint32 offset, quint32 bytes, QDataStream::ByteOrder bo) {
@@ -42,6 +53,40 @@ bool File::Write(quint32 offset, quint32 bytes, QDataStream::ByteOrder bo) {
     return true;
 }
 
+bool File::Write(quint32 offset, QByteArray bytes, QDataStream::ByteOrder bo) {
+
+    if(!this->Exists() || offset >= m_size)
+        return false;
+
+    QDataStream fout(m_file);
+    fout.setByteOrder(bo);
+
+    if(!m_file->seek(offset))
+        return false;
+
+    int amount = fout.writeRawData(bytes.constData(), bytes.size());
+    qDebug() << "Amount written: " + QString::number(amount);
+    if (!m_file->flush())
+        return false;
+
+    return true;
+}
+
+bool File::Append(QByteArray bytes, QDataStream::ByteOrder bo) {
+    if(!this->Exists() || !(m_openmode & QFile::Append))
+        return false;
+
+    QDataStream fout(m_file);
+    fout.setByteOrder(bo);
+
+    int amount = fout.writeRawData(bytes.constData(), bytes.size());
+    qDebug() << "Amount appended: " + QString::number(amount);
+    if (!m_file->flush())
+        return false;
+
+    return true;
+}
+
 quint32 File::Read(quint32 offset, QDataStream::ByteOrder bo) {
 
     quint32 bytes;
@@ -55,6 +100,23 @@ quint32 File::Read(quint32 offset, QDataStream::ByteOrder bo) {
         return 0xFFFFFFFF;
 
     fin >> bytes;
+    return bytes;
+}
+
+QByteArray* File::Read(quint32 offset, int size, QDataStream::ByteOrder bo) {
+
+    QByteArray* bytes = new QByteArray;
+    if(!this->Exists() || offset >= m_size)
+        return bytes;
+
+    QDataStream fin(m_file);
+    fin.setByteOrder(bo);
+
+    if(!m_file->seek(offset))
+        return bytes;
+
+    bytes->resize(size);
+    fin.readRawData(bytes->data(), size);
     return bytes;
 }
 
